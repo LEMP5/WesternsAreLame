f67b26d5a0c61cd985025791fc78c434d462cfce
Lisa Blaga <lisablaga@gmail.com>
2015-03-18 14:01:59 +0100

    Update Product.java

--- src/ModelLayer/Product.java
+++ src/ModelLayer/Product.java
@@ -1,5 +1,104 @@
 package ModelLayer;
 
 public class Product {
+	private String name;
+	private String type;
+	private String description;
+	private int purchasePrice;
+	private int salesPrice;
+	private int rentPrice;
+	private String country;
+	private int stock;
+	private int minStock;
+	
+
+
+	public Product(String name, String type, String description,
+			int purchasePrice, int salesPrice, int rentPrice, String country,
+			int stock, int minStock) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.purchasePrice = purchasePrice;
+		this.salesPrice = salesPrice;
+		this.rentPrice = rentPrice;
+		this.country = country;
+		this.stock = stock;
+		this.minStock = minStock;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(int purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public int getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(int salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public int getRentPrice() {
+		return rentPrice;
+	}
+
+	public void setRentPrice(int rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	
 
 }
