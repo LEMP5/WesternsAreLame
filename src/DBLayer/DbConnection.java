5fe6e77fcc9feaf948610137d7c2063b52e95047
Emil Illum Vandekerckhove <emil.vdk@gmail.com>
2015-03-19 10:58:23 +0100

    Here's the DbConnection

--- src/DBLayer/DbConnection.java
+++ src/DBLayer/DbConnection.java
@@ -3,24 +3,25 @@ package DBLayer;
 import java.sql.*;
 
 /**
- * @Author Kis Boisen Hansen
- * @Version 2006.10.02 - revised marts 2009 FEN
- * @version 2010.10.07 - new connection 
- * @version 2011.11.02 - new methods to handle transaction
- * This class is used to create the connection to the database
- * It is implemented as a singleton. The constructor is private to ensure that only
- * one object of the class is generated
+ * @Author Emil Illum Vandekerckhove
  * Version for Sql Server 2014 the database i located on kraka.ucn.dk
  */
 
 public class DbConnection
-{
-	private static final String  driver = "jdbc:sqlserver://localhost:1433";
-    private static final String  databaseName = ";databaseName=Company";
-    private static String  userName = ";user=sa";
-    private static String password = ";password=company";
-	private DatabaseMetaData metaData;
-    private static Connection connection;
+{   //Constants used to get access to the database
+	//SQL Server
+   private static final String  driver = "jdbc:sqlserver://kraka.ucn.dk:1433";
+ //  private static final String  driver = "jdbc:sqlserver://localhost:1433";
+   private static final String  databaseName = ";databaseName=dmaj0914_2Sem_5";
+    //SQL Server
+  //  private static String  userName = ";user=sa";
+    private static String  userName = "; user=dmaj0914_2Sem_5";
+    private static String password = ";password=IsAllowed";
+   
+  
+    private DatabaseMetaData dma;
+    private static Connection con;
+    // an instance of the class is generetated
     private static DbConnection  instance = null;
 
     // the constructor is private to ensure that only one object of this class is created
@@ -28,92 +29,88 @@ public class DbConnection
     {
     	String url = driver + databaseName + userName + password;
 
-        try
-        {
-        	//load of driver SQL Server
+        try{
+            //load af driver
+            //SQL Server
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            System.out.println("Load of class is ok");
+            System.out.println("Load of class ok");
+          
         }
-        catch(Exception e)
-        {
+        catch(Exception e){
             System.out.println("Can not find the driver");
             System.out.println(e.getMessage());
-        }
-        try
-        {
+        }//end catch
+        try{
             //connection to the database
-            connection = DriverManager.getConnection(url);
+           
+            con = DriverManager.getConnection(url);
             //set autocommit
-            connection.setAutoCommit(true);
-            metaData = connection.getMetaData(); //get meta data
-            System.out.println("Connection to " + metaData.getURL());
-            System.out.println("Driver " + metaData.getDriverName());
-            System.out.println("Database product name " + metaData.getDatabaseProductName());
-        }
+            con.setAutoCommit(true);
+            dma = con.getMetaData(); // get meta data
+            System.out.println("Connection to " + dma.getURL());
+            System.out.println("Driver " + dma.getDriverName());
+            System.out.println("Database product name " + dma.getDatabaseProductName());
+        }//end try
         catch(Exception e){
 
             System.out.println("Problems with the connection to the database");
             System.out.println(e.getMessage());
             System.out.println(url);
-        }
+        }//end catch
     }//end  constructor
-    
-    public static void closeConnection() //closeDb: closes the connection to the database
+	   
+  //closeDb: closes the connection to the database
+    public static void closeConnection()
     {
-       	try
-       	{
-            connection.close();
+       	try{
+            con.close();
             System.out.println("The connection is closed");
         }
-         catch (Exception e)
-       	{
+         catch (Exception e){
             System.out.println("Error trying to close the database " +  e.getMessage());
-        }
-    }
-    public Connection getDBcon()
+         }
+    }//end closeDB
+		
+    //getDBcon: Get-method, returns the connection to the database
+    public  Connection getDBcon()
     {
-       return connection; //getDBcon: Get-method, returns the connection to the database
+       return con;
     }
+    //this method is used to get the instance of the connection
     public static DbConnection getInstance()
     {
         if (instance == null)
-        	instance = new DbConnection();
+        {
+          instance = new DbConnection();
+        }
         return instance;
     }
     public static void startTransaction()
-    {
-    	try
-    	{
-    		connection.setAutoCommit(false);
-    	}
-    	catch(Exception e)
-    	{
-        System.out.println("fail start transaction");
+    { try{
+        con.setAutoCommit(false);
+        }
+      catch(Exception e){
+        System.out.println("fejl start transaction");
         System.out.println(e.getMessage());
-    	}
+      }
     }
     public static void commitTransaction()
-    {
-    	try
-    	{
-        connection.setAutoCommit(true);
+    { try{
+        con.setAutoCommit(true);
         }
-    	catch(Exception e)
-    	{
-        System.out.println("fail commit transaction");
+      catch(Exception e){
+        System.out.println("fejl commit transaction");
         System.out.println(e.getMessage());
-    	}
+      }
     }
     public static void rollbackTransaction()
-    {
-    	try
-    	{
-        connection.rollback();
-        connection.setAutoCommit(true);
+    { try{
+        con.rollback();
+        con.setAutoCommit(true);
         }
-    	catch(Exception e){
-        System.out.println("fail rollback transaction");
+      catch(Exception e){
+        System.out.println("fejl rollback transaction");
         System.out.println(e.getMessage());
-    	}
+      }
     }
 }//end DbConnection
